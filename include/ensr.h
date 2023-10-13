e82dfbfcd1f9de5006f1fd697a441a8b89cad5cb
Lukas Krickl <lukas@krickl.dev>
2023-10-13 21:55:23 +0200

    WIP: Basic functions

--- include/ensr.h
+++ include/ensr.h
@@ -1,10 +1,76 @@
-#ifndef ENSR_H__
-#define ENSR_H__
+#ifndef ENSR_H_
+#define ENSR_H_
+
+/**
+ * =============================
+ * Configuration
+ * Enable and disable features
+ * dynamically here
+ * =============================
+ */
+
+// Optional modules
+#define ENSR_MOD_IN_PATH 1
+#define ENSR_MOD_PATH_EXISTS 1
+#define ENSR_MOD_PROC_RUNNING 1
+#define ENSR_MOD_PID_RUNNING 1
+#define ENSR_MOD_PATH_OWNER 1
+
+// Config variables
+#define ENSR_CFG_FMT_OK ""
+#define ENSR_CFG_FMT_WARN ""
+#define ENSR_CFG_FMT_ERR ""
+#define ENSR_CFG_FMT_OFF ""
+
+// env variables
+#define ENSR_ENV_FMT_OK "ENSR_OK"
+#define ENSR_ENV_FMT_WARN "ENSR_WARN"
+#define ENSR_ENV_FMT_ERR "ENSR_ERR"
+#define ENSR_ENV_FMT_OFF "ENSR_FMT_OFF"
+
+/**
+ * =============================
+ * End configuration
+ * =============================
+ */
 
 struct ensr_config {
   _Bool verbose;
+
+  const char *rule_name;
+  const char *proc;
+  int pid;
+
+  const char *path;
 };
 
 int ensr_main(struct ensr_config *cfg);
 
-#endif 
+/**
+ * Checks that the lines received in stdin
+ * are exactly n lines
+ * This is useful for piping results from commands
+ * like grep into ensure
+ */
+int ensr_eqnlines(int nlines);
+int ensr_gtnlines(int nlines);
+int ensr_ltnlines(int nlines);
+
+/**
+ * Check if the name is a valid program
+ */
+int ensr_in_path(const char *path);
+
+/**
+ * Checks if file exists
+ */
+int ensr_path_exists(const char *path);
+
+int ensr_path_owner(const char *path, const char *usr);
+
+int ensr_proc_running(const char *proc_name);
+int ensr_pid_running(int pid);
+
+int ensr_platform_init(void);
+
+#endif
--- src/ensr.c
+++ src/ensr.c
@@ -1,5 +1,17 @@
 #include "ensr.h"
+#include <stdio.h>
 
 int ensr_main(struct ensr_config *cfg) {
+  if (ensr_platform_init() == -1) {
+    fprintf(stderr, "Platform init failed!\n");
+    return -1;
+  }
+
   return 0;
 }
+
+/**
+ * Platform specific code
+ */
+
+int ensr_platform_init(void) { return 0; }
